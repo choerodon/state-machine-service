775b5b7996930098ab3ace4a264505a96b395f75
陈士男 <shinan.chen@hand-china.com>
2018-09-27 23:40:13 +0800

    [IMP] 调整Feign配置

---
+++ src/main/java/io/choerodon/statemachine/infra/config/FeignClientAdaptor.java
@@ -0,0 +1,37 @@
+package io.choerodon.statemachine.infra.config;
+
+import feign.*;
+import feign.codec.Decoder;
+import feign.codec.Encoder;
+import io.choerodon.statemachine.infra.feign.CustomFeignClientAdaptor;
+import org.springframework.cloud.netflix.feign.FeignClientsConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * @author shinan.chen
+ * @date 2018/9/27
+ */
+@Configuration
+@Import(FeignClientsConfiguration.class)
+public class FeignClientAdaptor {
+    /**
+     * 配置自定义feign
+     *
+     * @param client
+     * @param decoder
+     * @param encoder
+     * @param interceptor
+     * @return
+     */
+    @Bean
+    public CustomFeignClientAdaptor InstanceServiceImpl(Client client, Decoder decoder, Encoder encoder, RequestInterceptor interceptor) {
+        CustomFeignClientAdaptor adaptor = Feign.builder().encoder(encoder).decoder(decoder)
+                .client(client)
+                .contract(new Contract.Default())
+                .requestInterceptor(interceptor)
+                .target(Target.EmptyTarget.create(CustomFeignClientAdaptor.class));
+        return adaptor;
+    }
+}
