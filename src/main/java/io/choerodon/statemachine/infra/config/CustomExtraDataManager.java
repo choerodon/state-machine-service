855e9aa27279af50a58aeed90ef333288816648a
陈士男 <shinan.chen@hand-china.com>
2018-11-13 21:44:42 +0800

    [ADD] ChoerodonExtraData

---
+++ src/main/java/io/choerodon/statemachine/infra/config/CustomExtraDataManager.java
@@ -0,0 +1,23 @@
+package io.choerodon.statemachine.infra.config;
+
+import io.choerodon.core.swagger.ChoerodonRouteData;
+import io.choerodon.swagger.annotation.ChoerodonExtraData;
+import io.choerodon.swagger.swagger.extra.ExtraData;
+import io.choerodon.swagger.swagger.extra.ExtraDataManager;
+
+/**
+ * @author shinan.chen
+ * @date 2018/11/13
+ */
+@ChoerodonExtraData
+public class CustomExtraDataManager implements ExtraDataManager {
+    @Override
+    public ExtraData getData() {
+        ChoerodonRouteData choerodonRouteData = new ChoerodonRouteData();
+        choerodonRouteData.setName("state");
+        choerodonRouteData.setPath("/state/**");
+        choerodonRouteData.setServiceId("state-machine-service");
+        extraData.put(ExtraData.ZUUL_ROUTE_DATA, choerodonRouteData);
+        return extraData;
+    }
+}
