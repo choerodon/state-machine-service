4a9815b43d8b7aa29ccff4ff6cbbf8cc1847b129
陈士男 <shinan.chen@hand-china.com>
2018-12-24 14:18:49 +0800

    [FIX] queryListTransform

--- src/main/java/io/choerodon/statemachine/api/service/impl/StateMachineConfigServiceImpl.java
+++ src/main/java/io/choerodon/statemachine/api/service/impl/StateMachineConfigServiceImpl.java
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -97,7 +98,7 @@ public class StateMachineConfigServiceImpl extends BaseServiceImpl<StateMachineC
             List<StateMachineConfigDTO> configDTOS = stateMachineConfigAssembler.toTargetList(configs, StateMachineConfigDTO.class);
             return configDTOS;
         }else{
-            return null;
+            return Collections.EMPTY_LIST;
         }
 
     }
