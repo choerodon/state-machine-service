3a9f30035c4195c436a364d89f7ba8f056563e47
陈士男 <shinan.chen@hand-china.com>
2019-04-10 14:54:51 +0800

    [ADD] 升级SpringBoot-1.5.x到2.0

--- src/main/java/io/choerodon/statemachine/infra/feign/UserFeignClient.java
+++ src/main/java/io/choerodon/statemachine/infra/feign/UserFeignClient.java
@@ -2,7 +2,7 @@ package io.choerodon.statemachine.infra.feign;
 
 import io.choerodon.statemachine.infra.feign.dto.ProjectDTO;
 import io.choerodon.statemachine.infra.feign.fallback.UserFeignClientFallback;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
